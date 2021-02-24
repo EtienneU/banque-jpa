9ffdd11c4253c56d10183fd4e32fc3a471feb66a
EtienneUrbano <urbano.etienne@laposte.net>
2021-02-24 12:23:13 +0100

    first commit

---
+++ src/fr/diginamic/banque/TestJpaBanque.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.diginamic.banque;
+
+/**
+ * @author urban
+ *
+ */
+public class TestJpaBanque {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
